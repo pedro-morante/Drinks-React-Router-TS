82e432dc3ac7dfdf8152270c121ca268ff75350c
Pedro Morante <pedro.morante@inchcape.com>
2024-10-17 08:11:35 -0500

    add drinks form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
+import { useMemo } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
-  const location = useLocation();
+  const { pathname } = useLocation();
+  const isHome = useMemo(() => pathname === "/", [pathname]);
 
   return (
     <header className="bg-slate-800">
@@ -33,6 +35,47 @@ export default function Header() {
             </NavLink>
           </nav>
         </div>
+        {isHome && (
+          <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Nombre o Ingredientes
+              </label>
+              <input
+                type="text"
+                id="ingredient"
+                name="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none"
+                placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila, Cafe"
+              />
+            </div>
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Categoria
+              </label>
+              <select
+                id="ingredient"
+                name="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none"
+              >
+                <option value="">-- Seleccione --</option>
+              </select>
+            </div>
+            <input
+              type="submit"
+              name=""
+              id=""
+              value="Buscar Recetas"
+              className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"
+            />
+          </form>
+        )}
       </div>
     </header>
   );
